e2e1aa2956fb6ea208f75f7bc4c8cda3094a73fa
robDevs <robbdevv@gmail.com>
2020-09-13 17:27:55 -0600

    create sudo code for controller class

--- controller.java
+++ controller.java
@@ -1,5 +1,26 @@
 public class controller {
     public static void main(String[] args) {
         System.out.println("stuff will happen here. Eg: choose new dog to give flea. ask if it should create a new dog. etc");
+
+        /** sudo sudo code. 
+         * it's like sudo code but even more lazy. 
+         * main(args) {
+         *      either ask user for how many dogs or take it from command line args. 
+         *      create array of that size of dogs. 
+         * 
+         * 
+         *      while(user doesn't quit) {
+         *              foreach(dog) {
+         *                  return = dog[i].doAction();
+         * 
+         *                  if(return != -1) {
+         *                          randomly select dog from list. 
+         *                          dog.addFlea(return);
+         *                  }
+         *              }
+         *          ask if user wants to quit. 
+         *      }
+         * }
+         */
     }
 }
