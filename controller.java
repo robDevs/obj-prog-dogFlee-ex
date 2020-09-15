c92fbbe76493e2582f631ef04f8df1ad7fd2556c
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2020-09-14 18:25:50 -0600

    Add files via upload

---
+++ controller.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class controller {
+
+    public void createDogs(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many dogs should be managed? ");
+        int iDogCount = sc.nextInt();
+
+        dog[] dogs = new dog[iDogCount]; // create array of dog objects from user input count
+
+        for (int i = 0; i < iDogCount; i++) { // instantiate each dog object in array
+            dogs[i] = new dog();
+
+            String sFriendly; // string will either be "not" or "" to identify whether object's friendly boolean is or isn't
+
+            if (dogs[i].friendly == true) {
+                sFriendly = "not ";
+            } else {
+                sFriendly = "";
+            }
+
+            System.out.println("Dog #" + i + " created and is " + sFriendly + "friendly.");
+        }
+
+        /*for (int i = 0; i < iDogCount; i++) {
+
+            System.out.println("Number of fleas: ?");
+
+            dogs[i].doAction();
+
+            System.out.println("");
+        }
+*/
+    }
+}
+
+/** sudo sudo code.
+ * it's like sudo code but even more lazy.
+ * main(args) {
+ *      either ask user for how many dogs or take it from command line args.
+ *      create array of that size of dogs.
+ *
+ *
+ *      while(user doesn't quit) {
+ *              foreach(dog) {
+ *                  return = dog[i].doAction();
+ *
+ *                  if(return != -1) {
+ *                          randomly select dog from list.
+ *                          dog.addFlea(return);
+ *                  }
+ *              }
+ *          ask if user wants to quit.
+ *      }
+ * }
+ */
